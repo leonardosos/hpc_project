53487ae49294ff435b393edfd554a92be5b2723d
leonardosos <leonardosos363@gmail.com>
2025-06-21 20:32:44 +0200

    script add

---
+++ submit/script_job_4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+#
+#PBS -N DAE  ! job name
+#PBS -j oe   !merge std-err and std-out
+#PBS -q s3par !queue
+#PBS -l nodes=1:ppn=16 # max 16 task, max 1 node
+#PBS -l walltime=00:10:00
+
+module purge
+module load oneapi/compiler
+module load oneapi/mkl
+module load oneapi/mpi
+module load IMPI/west5.4.0
+
+MYHOME="/home/corsohpc1/"
+DIR_JOB=${MYHOME}
+cd $DIR_JOB
+mpirun -np 4 python3 project.py > output_4.out
