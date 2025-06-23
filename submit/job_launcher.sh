39591ad4e63babaf81d9a1e2945644db07467da8
leonardosos <leonardosos363@gmail.com>
2025-06-23 18:15:31 +0200

    better automation

--- submit/job_launcher.sh
+++ submit/job_launcher.sh
@@ -2,25 +2,23 @@
 
 echo " -------- START ---------"
 
-while true; do
-    # Loop through numbers 1 to 16
-    for num in {1..16}; do
+# Loop through numbers 1 to 16
+for num in {1..16}; do
+    echo
+    echo
+    echo "Processing script_job_$num.sh"
+    
+    # Execute qsub 8 times in a row, twice for each number
+    for round in {1..3}; do
+        echo 
+        echo "Round $round for script_job_$num.sh"
         echo
-        echo
-        echo "Processing script_job_$num.sh"
-        
-        # Execute qsub 8 times in a row, twice for each number
-        for round in {1..2}; do
-            echo 
-            echo "Round $round for script_job_$num.sh"
-            echo
-            for i in {1..8}; do
-                echo "Submit script_job_$num.sh (round $round, submission $i)"
-                qsub script_job_"$num".sh
-            done
-        sleep 45
+        for i in {1..8}; do
+            echo "Submit script_job_$num.sh (round $round, submission $i)"
+            qsub script_job_"$num".sh
         done
+    sleep 45
     done
-
-    echo " -------- DONE ---------"
 done
+
+echo " -------- DONE ---------"
