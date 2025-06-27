6aed293774d90cc03623fda4f9afa90b3b5780d0
leonardosos <leonardosos363@gmail.com>
2025-06-27 18:10:16 +0200

    fix minot

--- submit/job_launcher_single.sh
+++ submit/job_launcher_single.sh
@@ -3,15 +3,35 @@
 echo " -------- START ---------"
 
 # Loop through numbers 1 to 16
-for num in {1..16}; do
+for num in {1..5}; do
     echo
     echo "Submit script_job_$num.sh"
 
     qsub script_job_"$num".sh
-    
-    # Wait 45 seconds 
-    sleep 45
         
 done
+
+# Wait 45 seconds 
+sleep 45
+
+for num in {6..12}; do
+    echo
+    echo "Submit script_job_$num.sh"
+
+    qsub script_job_"$num".sh
+        
+done
+
+# Wait 45 seconds 
+sleep 45
+
+for num in {12..16}; do
+    echo
+    echo "Submit script_job_$num.sh"
+
+    qsub script_job_"$num".sh
+        
+done
+
 
 echo " -------- DONE ---------"
